d6f2e87bdc5c76245d54054c6944a9711ccf05a1
plethorapanther <97998646+plethorapanther@users.noreply.github.com>
2023-09-05 11:48:08 -0600

    Cleaned up the file

--- TeamPythonFile.py
+++ TeamPythonFile.py
@@ -1,9 +1,5 @@
 print("Team: Manoa Chocolate")
-<<<<<<< HEAD
 print("Kacy Horning")
 print("Bracken Brinkel")
-=======
 print("Team: Junmin testing")
-print("Kacy Horning")
 
->>>>>>> 1a71666c0f1ed265eece935e98f817bd00024740
